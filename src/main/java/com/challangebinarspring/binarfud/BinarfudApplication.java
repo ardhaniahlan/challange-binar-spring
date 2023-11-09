448227bb5c2ed21c89f675d5077c730aab7942f9
ardhaniahlan <ardhan.official16@gmail.com>
2023-11-09 20:06:24 +0700

    fileupload

--- src/main/java/com/challangebinarspring/binarfud/BinarfudApplication.java
+++ src/main/java/com/challangebinarspring/binarfud/BinarfudApplication.java
@@ -1,9 +1,14 @@
 package com.challangebinarspring.binarfud;
 
+import com.challangebinarspring.binarfud.controller.fileupload.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class BinarfudApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/challangebinarspring/binarfud/controller/fileupload/ResourceConfig.java
@@ -0,0 +1,22 @@
+package com.challangebinarspring.binarfud.controller.fileupload;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods()
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+
+    @Override // cara call engpoint : localhost:9090/api/showFile/namafile.png:
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/uploads/**").addResourceLocations("file:uploads/");
+        registry.addResourceHandler("/showFile/**").addResourceLocations("file:cdn/");
+    }
+}
