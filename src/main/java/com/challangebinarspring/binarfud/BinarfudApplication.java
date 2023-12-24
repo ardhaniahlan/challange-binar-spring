95ce52f5cff6bac7828b9fff9350d48bcb04f5f7
ardhaniahlan <ardhan.official16@gmail.com>
2023-12-24 22:51:09 +0700

    cb8

--- src/main/java/com/challangebinarspring/binarfud/BinarfudApplication.java
+++ src/main/java/com/challangebinarspring/binarfud/BinarfudApplication.java
@@ -4,11 +4,13 @@ import com.challangebinarspring.binarfud.controller.fileupload.FileStorageProper
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
+@EnableTransactionManagement
 public class BinarfudApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/challangebinarspring/binarfud/service/impl/MerchantImpl.java
+++ src/main/java/com/challangebinarspring/binarfud/service/impl/MerchantImpl.java
@@ -9,11 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+@Transactional
 @Service
 @Slf4j
 public class MerchantImpl implements MerchantService {
